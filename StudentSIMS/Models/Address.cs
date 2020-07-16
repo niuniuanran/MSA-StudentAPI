f4edbee0a509bdade79f843dfd82a81c576c341c
Anran Niu <aniu423@UoA.auckland.ac.nz>
2020-07-16 19:01:36 +1200

    Add Address model, add into context

---
+++ StudentSIMS/Models/Address.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentSIMS.Models
+{
+    public class Address
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AddressId { get; set; }
+
+        [ForeignKey("Student")]
+        public int StudentId { get; set; }
+
+        public short StreetNumber { get; set; }
+        public string Street { get; set; }
+        public string Suburb { get; set; }
+        public string City { get; set; }
+        public short Postcode { get; set; }
+        public string Country { get; set; }
+
+    }
+}
