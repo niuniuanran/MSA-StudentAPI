c60dfbeeb004033ed8b9759c18001295346f8dbf
Anran Niu <aniu423@UoA.auckland.ac.nz>
2020-07-16 20:12:35 +1200

    Follow the MSA example to include Migrations

---
+++ StudentSIMS/Migrations/20200716061647_AddAddress.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentSIMS.Migrations
+{
+    public partial class AddAddress : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    AddressId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<int>(nullable: false),
+                    StreetNumber = table.Column<short>(nullable: false),
+                    Street = table.Column<string>(nullable: true),
+                    Suburb = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    Postcode = table.Column<short>(nullable: false),
+                    Country = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.AddressId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Address");
+        }
+    }
+}
