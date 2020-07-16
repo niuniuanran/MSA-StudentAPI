788bb6f8b6192c0aeca8c591783adcd9332eb76c
Anran Niu <aniu423@UoA.auckland.ac.nz>
2020-07-16 19:02:00 +1200

    Add controller for address

---
+++ StudentSIMS/Controllers/AddressesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentSIMS.Data;
+using StudentSIMS.Models;
+
+namespace StudentSIMS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressesController : ControllerBase
+    {
+        private readonly StudentContext _context;
+
+        public AddressesController(StudentContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Addresses
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Address>>> GetAddress()
+        {
+            return await _context.Address.ToListAsync();
+        }
+
+        // GET: api/Addresses/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Address>> GetAddress(int id)
+        {
+            var address = await _context.Address.FindAsync(id);
+
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            return address;
+        }
+
+        // PUT: api/Addresses/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAddress(int id, Address address)
+        {
+            if (id != address.AddressId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(address).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AddressExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Addresses
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Address>> PostAddress(Address address)
+        {
+            _context.Address.Add(address);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAddress", new { id = address.AddressId }, address);
+        }
+
+        // DELETE: api/Addresses/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Address>> DeleteAddress(int id)
+        {
+            var address = await _context.Address.FindAsync(id);
+            if (address == null)
+            {
+                return NotFound();
+            }
+
+            _context.Address.Remove(address);
+            await _context.SaveChangesAsync();
+
+            return address;
+        }
+
+        // POST: api/Addresses/forstudent/5
+        [HttpPost("forstudent/{studentId}")]
+        public async Task<ActionResult<Address>> PostAddressByStudentId(int studentId, Address address)
+        {
+            if (!StudentExists(studentId) || address.StudentId != studentId)
+            {
+                return BadRequest();
+            }
+
+            _context.Address.Add(address);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAddress", new { id = address.AddressId }, address);
+        }
+
+
+        // PUT: api/Addresses/forstudent/5
+        [HttpPut("forstudent/{studentId}")]
+        public async Task<ActionResult<Address>> PutAddressByStudentId(int studentId, Address address)
+        {
+            if (studentId != address.StudentId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(address).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AddressExists(address.AddressId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool AddressExists(int id)
+        {
+            return _context.Address.Any(e => e.AddressId == id);
+        }
+        private bool StudentExists(int id)
+        {
+            return _context.Student.Any(e => e.StudentId == id);
+        }
+    }
+}
