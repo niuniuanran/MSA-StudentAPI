846ab19400eb2d1cc85746ac837ae223c1fa00c4
Anran Niu <aniu423@UoA.auckland.ac.nz>
2020-07-16 16:53:48 +1200

    Add dbContext to program

--- StudentSIMS/Startup.cs
+++ StudentSIMS/Startup.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using StudentSIMS.Data;
 
 namespace StudentSIMS
 {
@@ -24,9 +26,12 @@ namespace StudentSIMS
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration.GetConnectionString("schoolSIMSConnection");
+            services.AddDbContext<StudentContext>(options => options.UseSqlServer(connection));
+           
+            services.AddCors();
             services.AddControllers();
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
